abca89878ae017703e86904588c57d4c8a3253f6
mjcarant0 <marjoycaranto@gmail.com>
2025-04-26 17:44:18 +0800

    Update test script to check channel and volume setting on two TVs

--- test_tv.py
+++ test_tv.py
@@ -6,9 +6,11 @@ def main():
         
     tv1.turnOn() # turn on tv1
     tv1.setChannel(30) # setting the channel
+    tv1.setVolume(3) # setting the volume level
     
     tv2.turnOn() # turn on tv2
     tv2.setChannel(3) # setting the channel
+    tv2.setVolume(2) # setting the volume level
 
 if __name__ == "__main__": # ensures it's working
     main()
