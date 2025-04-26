e5eb4fa4af0c60f596ce11e852451e492876c53a
mjcarant0 <marjoycaranto@gmail.com>
2025-04-26 17:07:31 +0800

    Add TV Class with channel, volume level, and power state

---
+++ tv.py
@@ -0,0 +1,6 @@
+class TV:
+    def __init__(self, channel, volumeLevel, on):
+        # instance variables
+        self.channel = channel
+        self.volumeLevel = volumeLevel
+        self.on = on
