8cc85cd84c4b6848a6979e5644665c6a6f06b380
mjcarant0 <marjoycaranto@gmail.com>
2025-04-26 17:35:55 +0800

    Update test script to check channel setting on two TVs

--- test_tv.py
+++ test_tv.py
@@ -1,12 +1,14 @@
-from tv import TV
+from tv import TV # import tv class
     
 def main():
-    tv1 = TV()
-    tv2 = TV()
+    tv1 = TV() # tv no. 1
+    tv2 = TV() # tv no. 2
         
-    tv1.turnOn()
-        
-    tv2.turnOn()
+    tv1.turnOn() # turn on tv1
+    tv1.setChannel(30) # setting the channel
+    
+    tv2.turnOn() # turn on tv2
+    tv2.setChannel(3) # setting the channel
 
-if __name__ == "__main__":
+if __name__ == "__main__": # ensures it's working
     main()
