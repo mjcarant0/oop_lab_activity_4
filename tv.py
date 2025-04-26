17bbaa46e25ebd1a3f60a5adaf4fb7fb6646f6ea
mjcarant0 <marjoycaranto@gmail.com>
2025-04-26 17:09:46 +0800

    Add default values for channel, volume, and power state

--- tv.py
+++ tv.py
@@ -1,5 +1,5 @@
 class TV:
-    def __init__(self, channel, volumeLevel, on):
+    def __init__(self, channel=1, volumeLevel=1, on=False):
         # instance variables
         self.channel = channel
         self.volumeLevel = volumeLevel
